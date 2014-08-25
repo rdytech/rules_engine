d5e51cc34de36857927459a5b60483c11f2371ba
Andrew McNamara <andrewm@jobready.com.au>
2014-08-25 10:59:50 +1000

    Add reference to outcome.

--- lib/rules_engine/outcome.rb
+++ lib/rules_engine/outcome.rb
@@ -1,8 +1,9 @@
 class RulesEngine::Outcome
-  attr_accessor :name, :values
+  attr_accessor :name, :values, :reference
 
   def initialize(options = {})
     @name = options.fetch(:name)
     @values = options.fetch(:values)
+    @reference = options.fetch(:reference, nil)
   end
 end
