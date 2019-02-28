981d5fecf6abf4fe3fb9f242affdbb0d3fb9dd18
Michael MacDonald <michael@starclass.com.au>
2019-02-28 16:28:51 +1100

    Remove codeclimate-test-reporter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,6 @@
 require 'simplecov'
 SimpleCov.start
 
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
-
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
   SimpleCov::Formatter::HTMLFormatter
 ]
