4232400ee90b65e268b6f1f461cc05f1412bb8f8
Michael MacDonald <michael@starclass.com.au>
2021-07-12 18:21:09 +1000

    WIP: multiple outcomes prototype working

--- lib/rules_engine/condition.rb
+++ lib/rules_engine/condition.rb
@@ -4,7 +4,11 @@ class RulesEngine::Condition
                 :when_true,
                 :when_false,
                 :original,
-                :override
+                :override,
+                :when_true_outcome,
+                :when_false_outcome,
+                :when_true_condition,
+                :when_false_condition
 
   def initialize(options = {})
     @name = options.fetch(:name)
@@ -13,6 +17,10 @@ class RulesEngine::Condition
     @when_false = options.fetch(:when_false, nil)
     @original = options.fetch(:original, nil)
     @override = options.fetch(:override, nil)
+    @when_true_outcome = options.fetch(:when_true_outcome, nil)
+    @when_false_outcome = options.fetch(:when_false_outcome, nil)
+    @when_true_condition = options.fetch(:when_true_condition, nil)
+    @when_false_condition = options.fetch(:when_false_condition, nil)
   end
 
   def override?
@@ -29,6 +37,20 @@ class RulesEngine::Condition
 
   def outcome(which)
     which ? when_true : when_false
+  end
+
+  def outcome_multiple(which)
+    which ? when_true_condition : when_false_condition
+  end
+
+  # for multiple outcomes
+  # might need to return a hash instead so that order is not important
+  def outcomes(which)
+    if which
+      [when_true_outcome, when_true_condition]
+    else
+      [when_false_outcome, when_false_condition]
+    end
   end
 end
 
--- lib/rules_engine/walker.rb
+++ lib/rules_engine/walker.rb
@@ -20,20 +20,38 @@ class RulesEngine::Walker
     logger.info("Executing Rule Set #{set.name}")
 
     node = set.root
+    outcome = nil
+    condition = nil
+    outcomes = []
+
     loop do
-      unless node.is_a?(RulesEngine::Condition)
-        add_event(set, node, nil) if node
-        return node
+      if set.multiple_outcomes?
+        if outcome
+          add_event(set, outcome, nil)
+          outcomes << outcome
+        end
+        return outcomes unless node
+      else
+        unless node.is_a?(RulesEngine::Condition)
+          add_event(set, node, nil) if node
+          return node
+        end
       end
 
       which = node.execute(context)
       logger.info("Evaluated condition #{node.condition}, result is #{which} (overridden: #{node.override?})")
       add_event(set, node, which)
-      node = node.outcome(which)
+
+      if set.multiple_outcomes?
+        outcome, node = node.outcomes(which)
+      else
+        node = node.outcome(which)
+      end
     end
   end
 
   def add_event(set, node, which)
     event_logger.add_event(set, node, which) if event_logger
+    # puts "Add Event - set: #{set.name}, node: #{node.name}, which: #{which}"
   end
 end
